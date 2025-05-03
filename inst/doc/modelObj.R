5cef933403ba751b79e0bd6ded12cffe9abda892
Shannon T. Holloway <shannon.t.holloway@gmail.com>
2025-05-03 21:40:02 +0000

    version 4.3

--- inst/doc/modelObj.R
+++ inst/doc/modelObj.R
@@ -1,15 +1,11 @@
-### R code from vignette source 'modelObj.Rnw'
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
 
-###################################################
-### code chunk number 1: modelObj.Rnw:185-187
-###################################################
+## -----------------------------------------------------------------------------
 library(modelObj)
 object1 <- buildModelObj(model=~x1, solver.method='lm')
 
-
-###################################################
-### code chunk number 2: modelObj.Rnw:195-211
-###################################################
+## -----------------------------------------------------------------------------
 mylm <- function(X,Y){
   obj <- list()
   obj$lm <- lm.fit(x=X, y=Y)
@@ -27,10 +23,7 @@ predict.mylm <- function(obj,data=NULL){
   return(obj)
 }
 
-
-###################################################
-### code chunk number 3: modelObj.Rnw:219-225
-###################################################
+## -----------------------------------------------------------------------------
 object2 <- buildModelObj(model = ~x1, 
                          solver.method = mylm, 
                          solver.args = list('X' = "x", 'Y' = "y"),
@@ -38,16 +31,10 @@ object2 <- buildModelObj(model = ~x1,
                          predict.args = list('obj' = "object", 
                                              'data' = "newdata"))
 
-
-###################################################
-### code chunk number 4: modelObj.Rnw:329-330
-###################################################
+## -----------------------------------------------------------------------------
 summary(pressure)
 
-
-###################################################
-### code chunk number 5: modelObj.Rnw:339-354
-###################################################
+## -----------------------------------------------------------------------------
 exampleFun <- function(modelObj, data, Y){
 
     fitObj <- fit(object = modelObj, data = data, response = Y)
@@ -64,10 +51,7 @@ exampleFun <- function(modelObj, data, Y){
     return(list("fittedSq"=fitted, "coef"=cfs))
 }
 
-
-###################################################
-### code chunk number 6: modelObj.Rnw:360-367
-###################################################
+## -----------------------------------------------------------------------------
 ylog <- log(pressure$pressure)
 objlm <- buildModelObj(model = ~temperature,
                        solver.method = "lm",
@@ -76,10 +60,7 @@ objlm <- buildModelObj(model = ~temperature,
 fitObjlm <- exampleFun(objlm, pressure, ylog)
 print(fitObjlm$coef)
 
-
-###################################################
-### code chunk number 7: modelObj.Rnw:371-378
-###################################################
+## -----------------------------------------------------------------------------
 objnls <- buildModelObj(model = ~exp(a + b*temperature),
                         solver.method = "nls",
                         solver.args = list('start'=list(a=1, b=0.1)),
@@ -88,10 +69,7 @@ objnls <- buildModelObj(model = ~exp(a + b*temperature),
 fitObjnls <- exampleFun(objnls, pressure, pressure$pressure)
 print(fitObjnls$coef)
 
-
-###################################################
-### code chunk number 8: modelObj.Rnw:382-390
-###################################################
+## -----------------------------------------------------------------------------
 objectnew <- buildModelObj(model = ~temperature, 
                            solver.method = mylm, 
                            solver.args = list('X' = "x", 'Y' = "y"),
@@ -100,5 +78,4 @@ objectnew <- buildModelObj(model = ~temperature,
                                                'data'="newdata"))
 fitObjnew <- exampleFun(objectnew, pressure, ylog)
 print(fitObjnew$coef)
-
 
